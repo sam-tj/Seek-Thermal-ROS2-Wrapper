9214ca0c474f2a64c508b4f7127ab291ace71e3d
Linda TJ <sameer.tuteja05@gmail.com>
2023-06-19 22:11:23 +0200

    image compress

---
+++ ros2_package/src/seek_thermal_ros/launch/thermal_publisher_launch.launch.py
@@ -0,0 +1,24 @@
+from launch import LaunchDescription
+from launch_ros.actions import Node
+
+def generate_launch_description():
+    return LaunchDescription([
+        Node(
+            package='seek_thermal_ros',
+            executable='thermal_publisher',
+            name='thermal_publisher',
+        ),
+        Node(
+            package='image_transport',
+            executable='republish',
+            name='thermal_republish',
+            arguments=[  
+                'raw',
+                'raw'
+            ],
+            remappings=[
+                ('in', '/thermalImage'),
+                ('out','/thermalImage/compressed')
+            ]
+        ),           
+    ])
